c218e80b0b3f3fe1710813325dad0de57d18f532
Cesar Estrada <cesar@codigomovil.mx>
2023-11-10 16:33:00 -0600

    Update to modern standards

--- README.md
+++ README.md
@@ -1,5 +1,92 @@
-# Ti.Calendar
+# Titanium Calendar Widget - ti.calendar
 
-A simple calendar widget for Titanium Alloy.
+## Introduction
+`ti.calendar` is a Titanium widget designed to display a calendar with customizable styling and behavior. It provides an easy way to integrate a calendar into your Titanium mobile applications. This readme will guide you through the usage and customization options of the `ti.calendar` widget.
 
-##Coming soon
+## Features
+
+- Display a calendar with a month view.
+- Customize the appearance of the calendar, including fonts, colors, and more.
+- Specify active dates to highlight them on the calendar.
+- Define date range limits for the calendar.
+- Easily navigate between months.
+- Select and trigger events for specific dates.
+
+## Installation
+
+1. Copy the `ti.calendar` widget folder into your Titanium project's `app/widgets` directory.
+
+2. In your Alloy project's `config.json` file, add the widget to your dependencies:
+
+   ```json
+   "dependencies": {
+     "ti.calendar": "1.0"
+   }
+   ```
+
+3. In your Alloy XML view file, include the widget:
+
+   ```xml
+   <Widget id="calendar" src="ti.calendar" />
+   ```
+
+4. In your controller, initialize the widget and configure its properties using `$.args`:
+
+   ```javascript
+   $.calendar.init({
+     // Configuration options
+   });
+   ```
+
+## Usage
+
+### Configuration Options
+
+The `ti.calendar` widget can be customized using the `$.args` property. Here are the available configuration options:
+
+- `active_dates`: An array of dates to highlight as active.
+- `current_date`: The initial date to display on the calendar.
+- `min_date`: The minimum selectable date.
+- `max_date`: The maximum selectable date.
+- `fontFamily`: The font family for calendar text.
+- `dateTextColor`: Text color for regular dates.
+- `todayTextColor`: Text color for today's date.
+- `activePinColor`: Color for active date indicators.
+- `inactivePinColor`: Color for inactive date indicators.
+- `backgroundColor`: Background color for the calendar.
+- `todayBackgroundColor`: Background color for today's date.
+- `dateBackgroundColor`: Background color for regular dates.
+- `selectedBackgroundColor`: Background color for selected dates.
+- `fillMonth`: Whether to fill the entire month with date cells.
+- `enablePastDays`: Allow selection of past dates.
+- `allowInactiveSelection`: Allow selection of inactive dates.
+
+## Example
+
+Here's a basic example of how to use the `ti.calendar` widget:
+
+```javascript
+// Initialize the calendar
+$.calendar.init({
+  active_dates: ['2023-10-10', '2023-10-15'],
+  current_date: '2023-10-01',
+  min_date: '2023-09-01',
+  max_date: '2023-11-30',
+  fontFamily: 'Arial',
+  // Customize other properties as needed
+});
+
+// Event listener for date selection
+$.calendar.addEventListener('selected', function(e) {
+  console.log('Selected Date:', e.date);
+  console.log('Active:', e.active);
+});
+```
+
+## License
+
+This `ti.calendar` widget is open-source and licensed under the MIT License.
+
+## Support and Contribution
+
+If you have any questions or issues with this widget, please feel free to open an issue on the GitHub repository. Contributions and improvements are also welcome.
--- controllers/widget.js
+++ controllers/widget.js
@@ -1,268 +1,266 @@
+const ROWS = 6
+const COLUMNS = 7
+let current_page = 0
+const Moment = require('alloy/moment')
+Moment.locale(Ti.Locale.currentLanguage)
 
-var args = arguments[0] || {};
-var Moment = require('alloy/moment');
+// Init
+$.monthName.font = {
+  fontWeight: 'bold',
+  fontFamily: $.args.fontFamily
+}
 
-var ROWS = 6;
-var COLUMNS = 7;
+$.monthYear.font = {
+  fontWeight: 'light',
+  fontFamily: $.args.fontFamily
+}
 
-_.defaults(args, {
+_.defaults($.args, {
+  // Data
+  active_dates: [],
+  current_date: Moment(),
+  min_date: Moment().subtract(6, 'months'),
+  max_date: Moment().add(6, 'months'),
 
-	// Data
-	current_date: Moment(),
-	active_dates: [],
-	min_date: Moment().subtract(6, 'months'),
-	max_date: Moment().add(6, 'months'),
+  // Style
+  fontFamily: '',
 
-	// Style
-	backgroundColor: 'transparent',
-	dateBackgroundColor: 'transparent',
-	todayBackgroundColor: '#af80',
-	dateTextColor: '#fff',
-	todayTextColor: '#000',
-	activePinColor: '#f39911',
-	inactivePinColor: 'transparent',
-	selectedBackgroundColor: '#60f39911',
-	fontFamily: '',
+  dateTextColor: '#ffffff',
+  todayTextColor: '#000000',
 
-	// Behaviour
-	allowInactiveSelection: false,
-	fillMonth: false,
-	enablePastDays: false
+  activePinColor: '#f39911',
+  inactivePinColor: 'transparent',
 
-});
+  backgroundColor: 'transparent',
+  todayBackgroundColor: '#aaff8800',
+  dateBackgroundColor: 'transparent',
+  selectedBackgroundColor: '#60f39911',
 
-var active_dates = args.active_dates ? getMomentDates(args.active_dates) : [];
-var current_page = 0;
+  // Behaviour
+  fillMonth: false,
+  enablePastDays: false,
+  allowInactiveSelection: false
+})
 
-/////////////
-// Methods //
-/////////////
-
+// Methods
 function refreshArrows() {
-	$.leftBtn.opacity = current_page <= 0 ? 0.4 : 1;
-	$.rightBtn.opacity = current_page >= $.monthScroll.views.length - 1 ? 0.4 : 1;
+  $.leftBtn.opacity = current_page <= 0 ? 0.4 : 1
+  $.rightBtn.opacity = current_page >= $.monthScroll.views.length - 1 ? 0.4 : 1
 }
 
 function getDayLabels() {
-	var days = Moment.weekdaysMin();
-	days.push(days.shift()); // Moment week has Sunday at index 0
-	_.each(days, function(day, i) {
-		var width = Math.floor($.calendar.rect.width / COLUMNS);
-		var $label = $.UI.create('Label', {
-			classes: ['dayLabel'],
-			width: width,
-			text: day.charAt(0),
-			left: i * width,
-			font: {
-				fontFamily: args.fontFamily
-			}
-		});
+  let days = Moment.weekdaysMin()
+  days.push(days.shift()) // Moment week has Sunday at index 0
 
-		$.dayLabels.add($label);
-	});
-}
+  _.each(days, (day, i) => {
+    let width = Math.floor($.calendar.rect.width / COLUMNS)
 
-function getMomentDates(dates) {
-	return _.map(dates, function(date) {
-		return Moment(date);
-	});
+    let $label = $.UI.create('Label', {
+      width: width,
+      left: i * width,
+      text: day.charAt(0),
+      classes: ['dayLabel'],
+      font: { fontFamily: $.args.fontFamily }
+    })
+
+    $.dayLabels.add($label)
+  })
 }
 
 function isInMomentsList(date, dates) {
-	return _.find(dates, function(day) {
-		return date.isSame(day, 'day');
-	});
+  return _.find(dates, day => {
+    return date.isSame(day, 'day')
+  })
 }
 
 function getDayContainer(number) {
-	var $this = $.UI.create('View', {
-		classes: ['day'],
-		width: Math.floor($.monthScroll.rect.width / COLUMNS),
-		height: Math.floor($.monthScroll.rect.height / ROWS),
-		backgroundColor: args.dateBackgroundColor,
-		opacity: 1,
-		date: null,
-		active: null,
-	});
-	$this.add($.UI.create('Label', {
-		classes: ['dayNumber'],
-		color: '#fff',
-		text: number,
-		font: {
-			fontFamily: args.fontFamily
-		}
-	}));
-	$this.add($.UI.create('View', {
-		classes: ['dayDot'],
-		backgroundColor: 'transparent'
-	}));
+  let $this = $.UI.create('View', {
+    opacity: 1,
+    date: null,
+    active: null,
+    classes: ['day'],
+    backgroundColor: $.args.dateBackgroundColor,
+    height: Math.floor($.monthScroll.rect.height / ROWS),
+    width: Math.floor($.monthScroll.rect.width / COLUMNS)
+  })
 
-	return $this;
+  $this.add($.UI.create('Label', {
+    text: number,
+    color: '#ffffff',
+    classes: ['dayNumber'],
+    font: { fontFamily: $.args.fontFamily }
+  }))
+
+  $this.add($.UI.create('View', {
+    classes: ['dayDot'],
+    backgroundColor: 'transparent'
+  }))
+
+  return $this
 }
 
 function setItemDate($item, date) {
-	$item.date = date;
-	$item.children[0].text = date.date();
+  $item.date = date
+  $item.children[0].text = date.date()
 }
 
 function setItemActive($item, active) {
-	$item.active = active;
-	$item.children[1].backgroundColor = active ? args.activePinColor : args.inactivePinColor;
+  $item.active = active
+  $item.children[1].backgroundColor = active ? $.args.activePinColor : $.args.inactivePinColor
 }
 
 function setItemToday($item, is_today) {
-	$item.backgroundColor = is_today ? args.todayBackgroundColor : args.dateBackgroundColor;
-	$item.children[0].color = is_today ? args.todayTextColor : args.dateTextColor;
+  $item.children[0].color = is_today ? $.args.todayTextColor : $.args.dateTextColor
+  $item.backgroundColor = is_today ? $.args.todayBackgroundColor : $.args.dateBackgroundColor
 }
 
 function setItemCurrent($item, current) {
-	$item.opacity = current ? 1 : 0.5;
+  $item.opacity = current ? 1 : 0.5
 }
 
 function getMonthView(month, year) {
-	var month_rows = [];
-	var start_date = Moment().month(month).year(year).startOf('month').startOf('week');
-	var end_date = Moment().month(month).year(year).endOf('month').endOf('week');
+  // Month skeleton
+  let $month_view = $.UI.create('View', {
+    classes: ['month'],
+    month: month,
+    year: year,
+    backgroundColor: $.args.backgroundColor,
+    ready: false
+  })
 
-	// Month skeleton
-	var $month_view = $.UI.create('View', {
-		classes: ['month'],
-		month: month,
-		year: year,
-		backgroundColor: args.backgroundColor,
-		ready: false
-	});
+  // Month activity indicator
+  let $loader = Ti.UI.createActivityIndicator({
+    style: Ti.UI.ActivityIndicatorStyle.BIG,
+    center: { x: '50%', y: '50%' }
+  })
 
-	// Month activity indicator
-	var $loader = Ti.UI.createActivityIndicator({
-		style: Ti.UI.ActivityIndicatorStyle.BIG,
-		center: {
-			x: '50%',
-			y: '50%'
-		}
-	});
-	$month_view.add($loader);
-	$month_view.__loader = $loader;
-	$loader.show();
+  $month_view.add($loader)
+  $month_view.__loader = $loader
+  $loader.show()
 
-	return $month_view;
+  return $month_view
 }
 
 function buildMonth($month_view, dates) {
-	if (!$month_view || $month_view.ready) return;
-	var start_date = Moment().month($month_view.month).year($month_view.year).startOf('month').startOf('week');
-	var end_date = Moment().month($month_view.month).year($month_view.year).endOf('month').endOf('week');
-	var $days_container = Ti.UI.createView({
-		height: Ti.UI.FILL,
-		width: Ti.UI.FILL
-	});
+  if (!$month_view || $month_view.ready) {
+    return
+  }
 
-	// Separators
-	for (var i = 0; i < ROWS; i++) {
-		$days_container.add($.UI.create('View', {
-			classes: ['hr'],
-			top: (i+1) * Math.floor($.monthScroll.rect.height / ROWS)
-		}));
-	}
+  let start_date = Moment().month($month_view.month).year($month_view.year).startOf('month').startOf('week')
 
-	// Add day containers
-	for (var d = 0; d < ROWS*COLUMNS; d++) {
-		var curday = Moment(start_date).add(d, 'days');
-		// If fillMonth is disabled, add only this month's days
-		if (curday.month() === $month_view.month || args.fillMonth == true) {
-			var $curview = getDayContainer(curday.date());
-			var row = Math.floor(d/COLUMNS);
-			var col = d % COLUMNS;
+  let $days_container = Ti.UI.createView({
+    width: Ti.UI.FILL,
+    height: Ti.UI.FILL
+  })
 
-			setItemDate($curview, curday);
-			setItemActive($curview, isInMomentsList(curday, dates));
-			setItemCurrent($curview, !curday.isBefore(Moment(), 'day') || (args.enablePastDays == true && (curday.month() === $month_view.month)));
-			setItemToday($curview, curday.isSame(Moment(), 'day'));
+  // Separators
+  for (let i = 0; i < ROWS; i++) {
+    $days_container.add($.UI.create('View', {
+      classes: ['hr'],
+      top: (i + 1) * Math.floor($.monthScroll.rect.height / ROWS)
+    }))
+  }
 
-			$curview.top = row * ($curview.height);
-			$curview.left = col * ($curview.width);
+  // Add day containers
+  for (let d = 0; d < ROWS * COLUMNS; d++) {
+    let curday = Moment(start_date).add(d, 'days')
 
-			$days_container.add($curview);
-		}
-	}
+    // If fillMonth is disabled, add only this month's days
+    if (curday.month() === $month_view.month || $.args.fillMonth === true) {
+      let $curview = getDayContainer(curday.date())
+      let row = Math.floor(d / COLUMNS)
+      let col = d % COLUMNS
 
-	$month_view.add($days_container);
-	$month_view.ready = true;
-	$month_view.__loader.hide();
+      setItemDate($curview, curday)
+      setItemActive($curview, isInMomentsList(curday, dates))
+      setItemCurrent($curview, !curday.isBefore(Moment(), 'day') || ($.args.enablePastDays === true && (curday.month() === $month_view.month)))
+      setItemToday($curview, curday.isSame(Moment(), 'day'))
+
+      $curview.top = row * ($curview.height)
+      $curview.left = col * ($curview.width)
+
+      $days_container.add($curview)
+    }
+  }
+
+  $month_view.add($days_container)
+  $month_view.ready = true
+  $month_view.__loader.hide()
 }
 
 function buildCalendar() {
-	$.main.removeEventListener('postlayout', buildCalendar);
+  $.main.removeEventListener('postlayout', buildCalendar)
 
-	// Add top labels
-	getDayLabels();
-	// Create the calendar views
-	var curmonth_index = -1; var i = 0;
-	for (var m = Moment(args.min_date); m.diff(Moment(args.max_date)) <= 0; m.add(1, 'months')) {
-		if (m.isSame(Moment(), 'month')) curmonth_index = i;
-		var monthview = getMonthView(m.month(), m.year());
-		$.monthScroll.addView(monthview);
-		i++;
-	}
+  // Add top labels
+  getDayLabels()
 
-	$.monthScroll.currentPage = current_page = curmonth_index > 0 ? curmonth_index : 0;
-	refreshCalendarMonth(current_page);
-	refreshArrows();
+  // Create the calendar views
+  let i = 0
+  let curmonth_index = -1
+  for (let m = Moment($.args.min_date); m.diff(Moment($.args.max_date)) <= 0; m.add(1, 'months')) {
+    if (m.isSame(Moment(), 'month')) {
+      curmonth_index = i
+    }
+
+    $.monthScroll.addView(getMonthView(m.month(), m.year()))
+
+    i++
+  }
+
+  $.monthScroll.currentPage = current_page = curmonth_index > 0 ? curmonth_index : 0
+
+  refreshCalendarMonth(current_page)
+
+  refreshArrows()
 }
 
 function refreshCalendarMonth(m) {
-	var month_date = Moment().month($.monthScroll.views[m].month).year($.monthScroll.views[m].year);
-	
-	$.monthName.text = month_date.format('MMMM').toUpperCase();
-	$.monthYear.text = month_date.format('YYYY');
+  let month_date = Moment().month($.monthScroll.views[m].month).year($.monthScroll.views[m].year)
 
-	buildMonth($.monthScroll.views[m], args.active_dates);
+  $.monthYear.text = month_date.format('YYYY')
+  $.monthName.text = month_date.format('MMMM').toUpperCase()
 
-	if (current_page - 1 > -1) buildMonth($.monthScroll.views[m-1], args.active_dates);
-	if (current_page + 1 < 12) buildMonth($.monthScroll.views[m+1], args.active_dates);
+  buildMonth($.monthScroll.views[m], $.args.active_dates)
+
+  if (current_page - 1 > -1) {
+    buildMonth($.monthScroll.views[m - 1], $.args.active_dates)
+  }
+
+  if (current_page + 1 < 12) {
+    buildMonth($.monthScroll.views[m + 1], $.args.active_dates)
+  }
 }
 
-///////////////
-// Listeners //
-///////////////
+// Listeners
+function doScroll({ currentPage }) {
+  if (currentPage === current_page) {
+    return
+  }
 
-$.main.addEventListener('postlayout', buildCalendar);
+  current_page = currentPage
 
-$.monthScroll.addEventListener('scroll', function(e) {
-	if (e.currentPage === current_page) return;
-	current_page = e.currentPage;
-	refreshArrows();
-	refreshCalendarMonth(current_page);
-});
+  refreshArrows()
 
-$.monthScroll.addEventListener('click', function(e) {
-	if (!e.source.date || (!e.source.active && !args.allowInactiveSelection) || (args.enablePastDays == false && e.source.date.isBefore(Moment(), 'day'))) return;
+  refreshCalendarMonth(current_page)
+}
 
-	e.source.animate({ backgroundColor: args.selectedBackgroundColor, duration: 150, autoreverse: true });
+function clickMonthScroll({ source }) {
+  if (!source.date || (source.active && $.args.allowInactiveSelection) || ($.args.enablePastDays === false && source.date.isBefore(Moment(), 'day'))) {
+    return
+  }
 
-	$.trigger('selected', {
-		date: e.source.date,
-		active: e.source.active
-	});
-});
+  source.animate({ backgroundColor: $.args.selectedBackgroundColor, duration: 75, autoreverse: true }, () => {
+    $.trigger('selected', {
+      date: source.date,
+      active: source.active
+    })
+  })
+}
 
-$.leftBtn.addEventListener('click', function() {
-	$.monthScroll.movePrevious();
-});
+function movePrevious() {
+  $.monthScroll.movePrevious()
+}
 
-$.rightBtn.addEventListener('click', function() {
-	$.monthScroll.moveNext();
-});
-
-//////////
-// Init //
-//////////
-
-$.monthName.font = {
-	fontFamily: args.fontFamily,
-	fontWeight: 'bold'
-};
-$.monthYear.font = {
-	fontFamily: args.fontFamily,
-	fontWeight: 'light'
-};
+function moveNext() {
+  $.monthScroll.moveNext()
+}
